86d9570694b02e291ebf04c4555575b00c91d193
Antonio David Villegas Yeguas <advy99@gmail.com>
2018-06-29 17:26:13 +0200

    funcion palindromo

--- bot.py
+++ bot.py
@@ -6,9 +6,23 @@ def saludar(bot, update):
         'Hola {}'.format(update.message.from_user.first_name))
 
 
+def palindromo(bot, update):
+	texto = update.message.reply_to_message.text
+
+	if(texto == texto[::-1]):
+		update.message.reply_text(
+			'El texto: " {} " es un palindromo'.format(texto))
+	else:
+		update.message.reply_text(
+			'El texto: " {} " no es un palindromo'.format(texto))
+
+
+
 updater = Updater(TOKEN)
 
 updater.dispatcher.add_handler(CommandHandler('start', saludar))
+
+updater.dispatcher.add_handler(CommandHandler('palindromo',palindromo))
 
 updater.start_polling()
 updater.idle()
