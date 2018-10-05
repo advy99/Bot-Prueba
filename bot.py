a2d849ef4270e650406973dd53229c955309cbd4
Juan Ocaña Valenzuela <32340574+patchispatch@users.noreply.github.com>
2018-10-05 19:54:20 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,12 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from config import TOKEN
+import logging
+
+# Habilitar logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 def saludar(bot, update):
     update.message.reply_text(
@@ -22,24 +29,40 @@ def palindromo(bot, update):
 	else:
 		update.message.reply_text(
 			'El texto: " {} " no es un palindromo'.format(texto))
-		
+
 def owo(bot, update):
     update.message.reply_text(
         'OwO whats this? {}'.format(update.message.from_user.first_name))
 
+# Error handler
+def error(bot, update, error):
+	"""Log Errors caused by Updates."""
+	logger.warning('Update "%s" caused error "%s"', update, error)
+
+def main():
+	# Iniciar el bot
+	# Crear el EventHandler y pasarle el TOKEN de nuestro bot:
+	updater = Updater(TOKEN)
+
+	# Hacer que el dispatcher registre los handlers:
+	dp = updater.dispatcher
+
+	# Comandos:
+	dp.add_handler(CommandHandler('start', saludar))
+	dp.add_handler(CommandHandler('palindromo', palindromo))
+	dp.add_handler(CommandHandler('sed', sed))
+	dp.add_handler(CommandHandler('catastrofe', catastrofe))
+	dp.add_handler(CommandHandler('owo', owo))
 
 
-updater = Updater(TOKEN)
+	# Handler de errores:
+	dp.add_error_handler(error)
 
-updater.dispatcher.add_handler(CommandHandler('start', saludar))
+	# Iniciar el bot:
+	updater.start_polling()
 
-updater.dispatcher.add_handler(CommandHandler('palindromo',palindromo))
+	# Tener el bot activado hasta que se detenga con Ctrl+c:
+	updater.idle()
 
-updater.dispatcher.add_handler(CommandHandler('sed', sed))
-
-updater.dispatcher.add_handler(CommandHandler('catastrofe',catastrofe))
-
-updater.dispatcher.add_handler(CommandHandler('owo',owo))
-
-updater.start_polling()
-updater.idle()
+if __name__ == '__main__':
+	main()
